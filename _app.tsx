6d5fe94ff6497ff408a56a53204eb5a203022f37
sam <x38517489@gmail.com>
2025-02-15 21:25:13 +0800

    Initial commit

--- _app.tsx
+++ _app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+      <meta name="google-site-verification" content="zMatERrBVsLx0SYOEIdv-WN3ovW_tt3K4NM1eeLA75s" />
+      </Head>
       <DefaultSeo
         title="DuiWay｜對味交友 - 三觀智能配對，約會更合拍！"
         description="「還在煩惱如何找到理想對象？DuiWay｜對味交友透過獨家『三觀問卷』，智能推薦最合拍的靈魂伴侶。不只線上聊天，還能安排真實見面，讓交友更高效、更有品質。立即加入，開始你的愛情旅程！」"
